9815b26fff49e1b4d6d3e8cc29c79cff82149d9e
William Walter <wwalter@singleplatform.com>
2017-06-30 10:01:09 -0400

    adding ubuntu1604 env, removing python chromedriver test because this only manages selenium not chrome or chromedriver

--- tests/test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-chrome_options = webdriver.ChromeOptions()
-desired_capabilities = chrome_options.to_capabilities()
-driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", desired_capabilities=desired_capabilities)
-
-driver.get('https://google.com/xhtml')
-driver.quit()
